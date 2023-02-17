785ea63f39a6983536761cd2b259dc319b05c8a3
Lại Quân <115360011+quanyato@users.noreply.github.com>
2023-02-17 13:24:37 +0700

    Quan edit

--- dsaktn_1002.cpp
+++ dsaktn_1002.cpp
@@ -1,39 +1,40 @@
-#include<bits/stdc++.h>
-
+#include <bits/stdc++.h>
 using namespace std;
+
 void xuly()
 {
-	int n,k;
+	int n, k;
 	cin >> n >> k;
-	int a[k+1];
+	int a[k + 1];
 	for (int i = 1; i <= k; ++i)
 		cin >> a[i];
-	int i=k;
-//	while(i>0 && a[i] == n-k+1) i--;
-	while( a[i] == n-k+1) i--;
-	if (i<=0)
+	int i = k;
+	//	while(i>0 && a[i] == n-k+1) i--;
+	while (a[i] == n - k + i)
+		i--;
+	if (i <= 0)
 	{
-		for (int i = 1; i <= k; ++i)
-			cout << i << ' ';
-		cout << endl;
+		for (int j = 1; j <= k; ++j)
+			a[j] = j;
 	}
 	else
-	{	
+	{
 		a[i]++;
-		for(int j=i+1; j<=k; j++)
-			a[j]=a[j-1]+1;
-		}
-	for (int i=1;i<=k;i++)
+		for (int j = i + 1; j <= k; j++)
+			a[j] = a[j - 1] + 1;
+	}
+	for (int i = 1; i <= k; i++)
 		cout << a[i] << ' ';
 	cout << endl;
 }
 
-int main(){
+int main()
+{
 	int t;
 	cin >> t;
-	while(t--)
+	while (t--)
 	{
 		xuly();
-	}	
+	}
 	return 0;
 }
