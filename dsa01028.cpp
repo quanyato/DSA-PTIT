2e9d552d7344575ec9b08278787c3d0e3d700a97
Lại Quân <quanlb.b21cn607@stu.ptit.edu.vn>
2023-02-18 22:47:14 +0700

    Source array format

---
+++ dsa01028.cpp
@@ -0,0 +1,47 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void insertSort(int a[], int l, int r)
+{
+    for (int i = l; i <= r; i++)
+    {
+        int tmp = a[i];
+        int j = i - 1;
+        while (j >= 0 && a[j] > tmp)
+        {
+            a[j + 1] = a[j];
+            j--;
+        }
+        a[j + 1] = tmp;
+    }
+}
+void formatSrcArray(int &n, int a[])
+{
+    insertSort(a, 0, n - 1);
+    int i = 0;
+    while (i < n)
+    {
+        if (a[i] == a[i + 1])
+        {
+            for (int j = i + 1; j < n; j++)
+            {
+                a[j] = a[j + 1];
+            }
+            n--;
+            i--;
+        }
+        i++;
+    }
+}
+
+int main()
+{
+    int n, k;
+    cin >> n >> k;
+    int a[n];
+    for (int i = 0; i < n; i++)
+    {
+        cin >> a[i];
+    }
+    formatSrcArray(n, a);
+}
