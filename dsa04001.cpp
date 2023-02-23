52ac1bec557d4850ba57bae9e5497b6168cf654c
Lại Quân <quanlb.b21cn607@stu.ptit.edu.vn>
2023-02-23 10:49:57 +0700

    Them de bai

--- dsa04001.cpp
+++ dsa04001.cpp
@@ -1,3 +1,18 @@
+// LUY THUA
+// Cho số nguyên dương N và K. Hãy tính N^K modulo 10^9+7.
+
+// Input:
+// Dòng đầu tiên là số lượng bộ test T (T ≤ 20).
+// Mỗi test gồm 1 số nguyên N và K (1 ≤ N ≤ 1000, 1 ≤ K ≤ 109).
+// Output:
+// Với mỗi test, in ra đáp án trên một dòng.
+
+// Ví dụ:
+// Input:          Output
+// 2               8
+// 2 3             16
+// 4 2
+
 #include <bits/stdc++.h>
 using namespace std;
 int M = 1e9 + 7;
